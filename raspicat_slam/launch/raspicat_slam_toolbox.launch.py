e393fd5865735f8c45ced28e7f58909ddadfa57f
uhobeike <beike315@icloud.com>
2023-04-07 20:43:48 +0900

    Refactor raspicat_slam_toolbox.launch.py

--- raspicat_slam/launch/raspicat_slam_toolbox.launch.py
+++ raspicat_slam/launch/raspicat_slam_toolbox.launch.py
@@ -25,35 +25,19 @@ from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
-    slam_toolbox_params_file = LaunchConfiguration('slam_toolbox_params_file')
-    map_saver_params_file = LaunchConfiguration('map_saver_params_file')
+    namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
+    use_rviz = LaunchConfiguration('use_rviz')
     autostart = LaunchConfiguration('autostart')
+    slam_toolbox_params_file = LaunchConfiguration('slam_toolbox_params_file')
     use_respawn = LaunchConfiguration('use_respawn')
     log_level = LaunchConfiguration('log_level')
 
-    lifecycle_nodes = ['map_saver']
-
-    param_substitutions = {
-        'use_sim_time': use_sim_time}
-
-    configured_params = RewrittenYaml(
-        source_file=map_saver_params_file,
-        param_rewrites=param_substitutions,
-        convert_types=True)
-
-    declare_slam_toolbox_params_file = DeclareLaunchArgument(
-        'slam_toolbox_params_file',
-        default_value=os.path.join(get_package_share_directory("raspicat_slam"),
-                                   'config', 'param', 'slam_toolbox.param.yaml'),
-        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
+    declare_namespace = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Top-level namespace')
     
-    declare_map_saver_params_file = DeclareLaunchArgument(
-    'map_saver_params_file',
-    default_value=os.path.join(get_package_share_directory("raspicat_slam"),
-                                'config', 'param', 'map_saver.param.yaml'),
-    description='Full path to the ROS2 parameters file to use for the map_saver node')
-
     declare_use_sim_time = DeclareLaunchArgument(
         'use_sim_time',
         default_value='False',
@@ -62,6 +46,17 @@ def generate_launch_description():
     declare_autostart = DeclareLaunchArgument(
         'autostart', default_value='True',
         description='Automatically startup the nav2 stack')
+    
+    declare_arg_use_rviz = DeclareLaunchArgument(
+        'use_rviz',
+        default_value='true',
+        description='Set "true" to launch rviz.')
+
+    declare_slam_toolbox_params_file = DeclareLaunchArgument(
+        'slam_toolbox_params_file',
+        default_value=os.path.join(get_package_share_directory("raspicat_slam"),
+                                   'config', 'param', 'slam_toolbox.param.yaml'),
+        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
 
     declare_use_respawn = DeclareLaunchArgument(
         'use_respawn', default_value='False',
@@ -70,11 +65,17 @@ def generate_launch_description():
     declare_log_level = DeclareLaunchArgument(
         'log_level', default_value='info',
         description='log level')
-    
-    declare_arg_use_rviz = DeclareLaunchArgument(
-        'use_rviz',
-        default_value='true',
-        description='Set "true" to launch rviz.')
+
+    param_substitutions = {
+        'use_sim_time': use_sim_time}
+
+    configured_params = RewrittenYaml(
+        source_file=slam_toolbox_params_file,
+        root_key=namespace,
+        param_rewrites=param_substitutions,
+        convert_types=True)
+
+    lifecycle_nodes = ['map_saver']
 
     map_saver_server = Node(
             package='nav2_map_server',
@@ -96,14 +97,11 @@ def generate_launch_description():
                         {'node_names': lifecycle_nodes}])
 
     slam_toolbox = Node(
-        parameters=[
-          slam_toolbox_params_file,
-          {'use_sim_time': use_sim_time}
-        ],
         package='slam_toolbox',
         executable='async_slam_toolbox_node',
         name='slam_toolbox',
-        output='screen')
+        output='screen',
+        parameters=[configured_params],)
     
     rviz_config_file = os.path.join(get_package_share_directory('raspicat_slam'), 
                                     'config', 'rviz', 'slam_toolbox.rviz')
@@ -112,12 +110,12 @@ def generate_launch_description():
                      name='rviz2',
                      output='log',
                      arguments=['-d', rviz_config_file],
-                     condition=IfCondition(LaunchConfiguration('use_rviz')))
+                     condition=IfCondition(use_rviz))
 
     ld = LaunchDescription()
 
+    ld.add_action(declare_namespace)
     ld.add_action(declare_slam_toolbox_params_file)
-    ld.add_action(declare_map_saver_params_file)
     ld.add_action(declare_use_sim_time)
     ld.add_action(declare_autostart)
     ld.add_action(declare_use_respawn)
